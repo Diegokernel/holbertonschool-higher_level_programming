583eaf2086c6825ea8dca842e59248c0ed6380d9
Diegokernel <777@holbertonschool.com>
2019-05-09 22:11:13 -0500

    fixpepe

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-        changed_list = [replace if elem is search else elem for elem in my_list]
-        return changed_list
+        change_list = [replace if elem is search else elem for elem in my_list]
+        return change_list
