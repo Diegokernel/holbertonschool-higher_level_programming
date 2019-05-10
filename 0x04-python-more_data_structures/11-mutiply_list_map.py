ef547a47ad3d76989dcc711d40a5f90576f2a165
Diegokernel <777@holbertonschool.com>
2019-05-09 22:13:17 -0500

    lines

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    multiplied = list(map(lambda factor: factor * number, my_list))
-    return multiplied
+    return list(map(lambda factor: factor * number, my_list))
