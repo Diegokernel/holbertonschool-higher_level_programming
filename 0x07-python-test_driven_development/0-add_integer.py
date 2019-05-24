5a9bc4f418b72cd4ac7b080b5f12ca4491eec131
Diegokernel <47120841+Diegokernel@users.noreply.github.com>
2019-05-24 18:39:30 -0500

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,12 +5,11 @@
     a, b
 """
 
-
 def add_integer(a, b=98):
     """Returns a + b.
        checks typeErrors for a and b
     """
-if not isinstance(a, (float, int)):
+    if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
