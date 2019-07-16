89728e1c14a9360d620a92708eec893f6e3dd667
Diegokernel <47120841+Diegokernel@users.noreply.github.com>
2019-07-15 21:30:19 -0500

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS second_table
-(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (1, "Jhon", 10);
 INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (3, "Bob", 14);
