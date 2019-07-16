7c4af2a08b7aae1c49b98d46307f5c45d6d47cb6
Diegokernel <777@holbertonschool.com>
2019-07-15 21:18:35 -0500

    db

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city
+ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,4 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT city,AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR  month=8
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
