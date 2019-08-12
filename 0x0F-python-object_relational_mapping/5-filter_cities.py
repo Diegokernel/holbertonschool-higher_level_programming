bc3cbfc12aea6090839e9a9c75961afabc752844
Diegokernel <777@holbertonschool.com>
2019-08-12 10:10:14 -0500

    fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,13 +4,13 @@ from sys import argv
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1],
-            passwd=argv[2], db=argv[3], port=3306)
+                         passwd=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities\
             INNER JOIN states ON cities.state_id = states.id\
             WHERE states.name LIKE BINARY %s\
-            ORDER BY cities.id ASC;", (sys.argv[4],))
+            ORDER BY cities.id ASC;", (argv[4],))
     rows = cur.fetchall()
     items = []
     for row in rows:
