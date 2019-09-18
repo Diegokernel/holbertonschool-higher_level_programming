718d00f66d3c8ddd95e948a2ea5136e0a8c6e383
Diegokernel <777@holbertonschool.com>
2019-09-17 22:47:18 -0500

    fix square style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 
 const val = process.argv[2];
 if (Number(val)) {
-    for (let a = 0; a < val; a++) {
-	console.log('X'.repeat(val));
-    }
+  for (let a = 0; a < val; a++) {
+    console.log('X'.repeat(val));
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
