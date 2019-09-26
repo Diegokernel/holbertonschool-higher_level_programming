837eb6d606c7d62f6fd4f2a980c8ad36d986f3fc
Diegokernel <777@holbertonschool.com>
2019-09-25 23:24:15 -0500

    fix 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require('fs');
+const fs = require('request');
 fs(process.argv[2], (err, res) => {
   if (err) return console.log(err);
   console.log('code: %d', res.statusCode);
